ff6050eef4c264a8c48a2bad46fb827f2fe71648
Afif Al Mamun <afifanik@gmail.com>
2024-03-30 00:46:55 -0600

    Add model evaluator

---
+++ eval_model.py
@@ -0,0 +1,22 @@
+import torch
+from PIL import Image
+from torchvision import transforms
+
+from models.unet import UNet
+
+def load_checkpoint(model, checkpoint: str):
+    model.load_state_dict(torch.load(checkpoint))
+
+if __name__ == "__main__":
+    checkpoint = "/home/mdafifal.mamun/research/tiny_brains/unet.pth"
+    image_path = "/home/mdafifal.mamun/research/tiny_brains/assets/train_images/val/data/CC0002_philips_15_56_M.jpg"
+
+    model = UNet()
+    model.eval()
+
+    load_checkpoint(model, checkpoint=checkpoint)
+
+    image = Image.open(image_path).convert('L')
+    out = model(transforms.ToTensor()(image).unsqueeze(0))
+    print(out)
+
