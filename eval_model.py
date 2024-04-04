4d7029dc7b7c2a58dee3f3ceec32e3f1e594f5ce
saadashraf <saadbinashraf14@gmail.com>
2024-04-04 14:19:06 -0600

    Refined evaluation script.

--- eval_model.py
+++ eval_model.py
@@ -1,24 +1,42 @@
 import torch
 from PIL import Image
 from torchvision import transforms
+import os
+
+from tqdm import tqdm
 
 from models.unet import UNet
+
 
 def load_checkpoint(model, checkpoint: str):
     model.load_state_dict(torch.load(checkpoint))
 
-if __name__ == "__main__":
-    checkpoint = "/home/mdafifal.mamun/research/tiny_brains/unet.pth"
-    image_path = "/home/mdafifal.mamun/research/tiny_brains/assets/train_images/val/data/CC0002_philips_15_56_M.jpg"
 
+def predict(model, device, image_path, output_path):
+    image = Image.open(image_path).convert('L')
+    image = transforms.ToTensor()(image).unsqueeze(0)
+    image = image.to(device)
+    out = model(image)
+    out_image = transforms.ToPILImage()(out.squeeze(0))
+
+    out_image.save(output_path)
+
+
+if __name__ == "__main__":
+    curr_dir = os.getcwd()
+
+    checkpoint = os.path.join(curr_dir, "test_contrast_inverted.pth")
+    image_dir = os.path.join(curr_dir, "assets", "contrast_inverted_train_images", "test", "data")
+    output_dir = os.path.join(curr_dir, "assets", "model_outputs", "contrast_inverted")
+
+    images = os.listdir(image_dir)
+
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     model = UNet()
+    model = model.to(DEVICE)
     model.eval()
 
     load_checkpoint(model, checkpoint=checkpoint)
 
-    image = Image.open(image_path).convert('L')
-    out = model(transforms.ToTensor()(image).unsqueeze(0))
-    out_image = transforms.ToPILImage()(out.squeeze(0))
-
-    out_image.save("out_{}".format(image_path.split("/")[-1]))
-
+    for image in tqdm(images):
+        predict(model, DEVICE, os.path.join(image_dir, image), os.path.join(output_dir, image))
