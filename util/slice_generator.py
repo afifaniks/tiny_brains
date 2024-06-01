d463a9036779afcef54c6e5c28ba3b8e62a8c4d8
Afif Al Mamun <afifanik@gmail.com>
2024-05-31 23:20:07 -0600

    Add 2d image slice generator

---
+++ util/slice_generator.py
@@ -0,0 +1,59 @@
+import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import nibabel as nib
+import numpy as np
+from tqdm import tqdm
+
+
+def generate_2d_slices(source_dir, corrupted_dir):
+    source_2d = os.path.join(source_dir, "2d_slices")
+    corrupted_2d = os.path.join(corrupted_dir, "2d_slices")
+
+    os.makedirs(source_2d, exist_ok=True)
+    os.makedirs(corrupted_2d, exist_ok=True)
+
+    source_3d_images = sorted(map(str, Path(source_dir).glob("*nii")))
+    corrupted_3d_images = sorted(map(str, Path(corrupted_dir).glob("*nii")))
+
+    for source, corrupted in tqdm(
+        zip(source_3d_images, corrupted_3d_images), total=len(source_3d_images)
+    ):
+        assert source.split("/")[-1] == corrupted.split("/")[-1]
+
+        output_file_name = os.path.splitext(os.path.basename(source))[0]
+        print(f"Processing: {source}")
+
+        source_img = nib.load(source).get_fdata()
+        corrupted_img = nib.load(corrupted).get_fdata()
+
+        for i in range(source_img.shape[2]):
+            source_slice_2d = source_img[:, :, i]
+            corrupted_slice_2d = corrupted_img[:, :, i]
+
+            if np.any(source_slice_2d != 0):
+                plt.imsave(
+                    f"{source_2d}/{output_file_name}_slice_{i}.png",
+                    source_slice_2d,
+                    cmap="gray",
+                )
+                plt.imsave(
+                    f"{corrupted_2d}/{output_file_name}_slice_{i}.png",
+                    corrupted_slice_2d,
+                    cmap="gray",
+                )
+
+
+# For Afif
+destination_gt_train_data_dir = "/work/disa_lab/projects/tiny_brains/unet_segmentation/unet_segmentation/train/source_images_train"
+destination_gt_val_data_dir = "/work/disa_lab/projects/tiny_brains/unet_segmentation/unet_segmentation/val/source_images_val"
+
+destination_motion_train_data_dir = "/work/disa_lab/projects/tiny_brains/unet_segmentation/unet_segmentation/train/cc_motion_corrupted_train"
+destination_motion_val_data_dir = "/work/disa_lab/projects/tiny_brains/unet_segmentation/unet_segmentation/val/cc_motion_corrupted_val"
+
+print("Generate slices for train data...")
+generate_2d_slices(destination_gt_train_data_dir, destination_motion_train_data_dir)
+
+print("Generate slices for validation data...")
+generate_2d_slices(destination_gt_val_data_dir, destination_motion_val_data_dir)
