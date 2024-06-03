d3f6c4c0dc7537e3f0ba88ca864158ca0d6abcf6
Afif Al Mamun <afifanik@gmail.com>
2024-06-03 00:12:45 -0600

    Save images on metric generation

--- metrics_generator.py
+++ metrics_generator.py
@@ -5,9 +5,11 @@ import torch
 import torch.nn as nn
 from loguru import logger
 from torch.utils.data import DataLoader
-from torchmetrics.image import (PeakSignalNoiseRatio,
-                                StructuralSimilarityIndexMeasure,
-                                VisualInformationFidelity)
+from torchmetrics.image import (
+    PeakSignalNoiseRatio,
+    StructuralSimilarityIndexMeasure,
+    VisualInformationFidelity,
+)
 from torchvision import transforms
 from tqdm import tqdm
 
@@ -16,18 +18,35 @@ from models.unet import UNet
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Script to evaluate model on test dataset")
-    parser.add_argument("--test_image_dir", type=str, required=True, help="Path to the test image directory")
-    parser.add_argument("--test_label_dir", type=str, required=True, help="Path to the test label directory")
-    parser.add_argument("--checkpoint", type=str, required=True, help="Path to the checkpoint")
+    parser = argparse.ArgumentParser(
+        description="Script to evaluate model on test dataset"
+    )
+    parser.add_argument(
+        "--test_image_dir",
+        type=str,
+        required=True,
+        help="Path to the test image directory",
+    )
+    parser.add_argument(
+        "--test_label_dir",
+        type=str,
+        required=True,
+        help="Path to the test label directory",
+    )
+    parser.add_argument(
+        "--checkpoint", type=str, required=True, help="Path to the checkpoint"
+    )
     parser.add_argument("--runs", type=int, default=5, help="Number of runs")
     return parser.parse_args()
 
+
 args = parse_arguments()
 
-TRANSFORMATIONS = transforms.Compose([
-    transforms.ToTensor(),
-])
+TRANSFORMATIONS = transforms.Compose(
+    [
+        transforms.ToTensor(),
+    ]
+)
 
 # Prepare dataset
 logger.debug(f"Preparing datasets...")
@@ -36,11 +55,13 @@ logger.debug(f"Number of samples: {len(test_dataset)}")
 
 # Training parameters
 device = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 16
+BATCH_SIZE = 50
 
 # Data loaders
 logger.debug(f"Preparing dataloaders...")
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+test_loader = DataLoader(
+    test_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
+)
 
 # Model
 model = UNet()
@@ -50,11 +71,18 @@ model = model.to(device)
 metrics = {
     "psnr": PeakSignalNoiseRatio().to(device),
     "ssim": StructuralSimilarityIndexMeasure(data_range=1.0).to(device),
-    "vif": VisualInformationFidelity().to(device)
+    "vif": VisualInformationFidelity().to(device),
 }
 
-num_runs = args.runs 
+num_runs = args.runs
 all_scores = []
+
+
+def _save_images(images, names):
+    for image, name in zip(images, names):
+        image = transforms.ToPILImage()(image)
+        image.save("assets/test_outputs/{}.jpg".format(name))
+
 
 for run in range(num_runs):
     logger.debug(f"Run {run + 1}/{num_runs}")
@@ -66,15 +94,29 @@ for run in range(num_runs):
     model.eval()
     val_loss = 0.0
 
+    step = 0
+
     with torch.no_grad():
-        for inputs, targets in tqdm(test_loader, desc="Test step"):
+        for inputs, targets, file_names in tqdm(test_loader, desc="Test step"):
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = model(inputs)
+
+            if step % 10 == 0:
+                _save_images(
+                    [targets[0], inputs[0], outputs[0]],
+                    [
+                        f"{file_names[0]}_target",
+                        f"{file_names[0]}_input",
+                        f"{file_names[0]}_output",
+                    ],
+                )
 
             # Compute loss
             criterion = nn.MSELoss()
             loss = criterion(outputs, targets)
             val_loss += loss.item() * inputs.size(0)
+
+            step += 1
 
             # Compute metrics
             for metric_name, metric_fn in metrics.items():
