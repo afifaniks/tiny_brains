3bb525df8c8ecc9982fe70af054267c6ce222383
saadashraf <saadbinashraf14@gmail.com>
2024-08-28 22:27:26 -0600

    Fixed TV Loss bug.

--- util/model_util.py
+++ util/model_util.py
@@ -14,14 +14,14 @@ class TotalVariationLoss(nn.Module):
     def __init__(self):
         super(TotalVariationLoss, self).__init__()
 
-    def total_variation_loss(self, image):
-        tv_h = torch.mean(torch.abs(image[:, 1:, :] - image[:, :-1, :]))
-        tv_w = torch.mean(torch.abs(image[:, :, 1:] - image[:, :, :-1]))
+    def total_variation(self, image):
+        tv_h = torch.mean(torch.abs(image[:, :, :, 1:, :] - image[:, :, :, :-1, :]))
+        tv_w = torch.mean(torch.abs(image[:, :, :, :, 1:] - image[:, :, :, :, :-1]))
         return tv_h + tv_w
 
     def forward(self, output, target):
-        tv_output = self.total_variation_loss(output)
-        tv_target = self.total_variation_loss(target)
+        tv_output = self.total_variation(output)
+        tv_target = self.total_variation(target)
 
         loss = tv_output - tv_target
 
